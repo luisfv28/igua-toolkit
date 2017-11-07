5405cd4d4a1d8d38fdd79a3fbb0a8b5805085a72
kikomayorga <kiko@escuelab.org>
2017-11-07 00:55:36 -0500

    better flowmeter building proces

--- iguino_blocks/flowmeter/flowmeter.ino
+++ iguino_blocks/flowmeter/flowmeter.ino
@@ -1,142 +1,40 @@
-/*
-Liquid flow rate sensor -DIYhacking.com Arvind Sanjeev
+#include <FlowMeter.h>                  // https://github.com/sekdiy/FlowMeter
 
-Measure the liquid/water flow rate using this code. 
-Connect Vcc and Gnd of sensor to arduino, and the 
-signal line to arduino digital pin 2.
- 
- */
- 
- // 1 balde = 4 litros = 4:30 min
- 
- // 1 litro = 270 segundos / 4
- // 1 litro = 67.5 segundos
- // 0.x litros = 60 segundos
- 
- // x = 60 / 67.6 = 0.88 l/min
- 
-byte statusLed    = 13;
+// connect a flow meter to an interrupt pin (see notes on your Arduino model for pin numbers)
+FlowMeter Meter = FlowMeter(2);
 
-byte sensorInterrupt = 0;  // 0 = digital pin 2
-byte sensorPin       = 2;
+// set the measurement update period to 1s (1000 ms)
+const unsigned long period = 1000;
 
-// The hall-effect flow sensor outputs approximately 4.5 pulses per second per
-// litre/minute of flow.
-float calibrationFactor = 4.5;
+// define an 'interrupt service handler' (ISR) for every interrupt pin you use
+void MeterISR() {
+  // let our flow meter count the pulses
+  Meter.count();
+}
 
-volatile byte pulseCount;  
-
-float flowRate;
-unsigned int flowMilliLitres;
-unsigned long totalMilliLitres;
-unsigned long oldTotalMillilitres;
-unsigned long before;
-
-unsigned long oldTime;
-
-void setup()
-{
-  
-  // Initialize a serial connection for reporting values to the host
+void setup() {
+  // prepare serial communication
   Serial.begin(9600);
-   
-  // Set up the status LED line as an output
-  pinMode(statusLed, OUTPUT);
-  digitalWrite(statusLed, HIGH);  // We have an active-low LED attached
-  
-  pinMode(sensorPin, INPUT_PULLUP);
-  digitalWrite(sensorPin, HIGH);
 
-  pulseCount        = 0;
-  flowRate          = 0.0;
-  flowMilliLitres   = 0;
-  totalMilliLitres  = 0;
-  oldTotalMillilitres = 0;
-  oldTime           = 0;
-  before = millis();
+  // enable a call to the 'interrupt service handler' (ISR) on every rising edge at the interrupt pin
+  // do this setup step for every ISR you have defined, depending on how many interrupts you use
+  attachInterrupt(INT0, MeterISR, RISING);
 
-  // The Hall-effect sensor is connected to pin 2 which uses interrupt 0.
-  // Configured to trigger on a FALLING state change (transition from HIGH
-  // state to LOW state)
-  attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
+  // sometimes initializing the gear generates some pulses that we should ignore
+  Meter.reset();
 }
 
-/**
- * Main program loop
- */
-void loop()
-{
-   
-   if((millis() - oldTime) > 300)    // Only process counters once per second
-  { 
-    // Disable the interrupt while calculating flow rate and sending the value to
-    // the host
-    detachInterrupt(sensorInterrupt);
-        
-    // Because this loop may not complete in exactly 1 second intervals we calculate
-    // the number of milliseconds that have passed since the last execution and use
-    // that to scale the output. We also apply the calibrationFactor to scale the output
-    // based on the number of pulses per second per units of measure (litres/minute in
-    // this case) coming from the sensor.
-    flowRate = ((300.0 / (millis() - oldTime)) * pulseCount) / calibrationFactor;
-    
-    // Note the time this processing pass was executed. Note that because we've
-    // disabled interrupts the millis() function won't actually be incrementing right
-    // at this point, but it will still return the value it was set to just before
-    // interrupts went away.
-    oldTime = millis();
-    
-    // Divide the flow rate in litres/minute by 60 to determine how many litres have
-    // passed through the sensor in this 1 second interval, then multiply by 1000 to
-    // convert to millilitres.
-    flowMilliLitres = (flowRate / 60) * 1000;
-    
-    // Add the millilitres passed in this second to the cumulative total
+void loop() {
+  // wait between output updates
+  delay(period);
 
-    oldTotalMillilitres = totalMilliLitres;
-    totalMilliLitres += flowMilliLitres;
-      
-    unsigned int frac;
-    
-    // Print the flow rate for this second in litres / minute
-    // Serial.print("Flow rate: ");
-    // Serial.print(int(flowRate));  // Print the integer part of the variable
-    // Serial.print(".");             // Print the decimal point
-    // Determine the fractional part. The 10 multiplier gives us 1 decimal place.
-    frac = (flowRate - int(flowRate)) * 10;
-    // Serial.print(frac, DEC) ;      // Print the fractional part of the variable
-    // Serial.print("L/min");
-    // Print the number of litres flowed in this second
-    // Serial.print("  Current Liquid Flowing: ");             // Output separator
-    // Serial.print(flowMilliLitres);
-    // Serial.print("mL/Sec");
+  // process the (possibly) counted ticks
+  Meter.tick(period);
 
-    // Print the cumulative total of litres flowed since starting
-    // Serial.print("  Output Liquid Quantity: ");             // Output separator
-    Serial.println(totalMilliLitres);
-    if (oldTotalMillilitres != totalMilliLitres)
-      {
-      before = millis();
-      }
-    if ((millis() - before) > 43200000 ) //     12 horas sin uso?
-      {totalMilliLitres = 0;}
-      // Serial.println("mL");
-    
- 
+  // output some measurement result
+  Serial.println("Currently " + String(Meter.getCurrentFlowrate()) + " l/min, " + String(Meter.getTotalVolume())+ " l total.");
 
-    // Reset the pulse counter so we can start incrementing again
-    pulseCount = 0;
-    
-    // Enable the interrupt again now that we've finished sending output
-    attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
-  }
-}
-
-/*
-Insterrupt Service Routine
- */
-void pulseCounter()
-{
-  // Increment the pulse counter
-  pulseCount++;
+  //
+  // any other code can go here
+  //
 }
